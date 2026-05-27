57c88a9f11552718edc4ca17667e84c7520057a2
Niiaks <napappoe007@st.ug.edu.gh>
2026-05-27 21:26:39 +0000

    load .env with name, url and threshold flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
+	"time"
 
 	db2 "github.com/Niiaks/price-tracker/internal/db"
+	"github.com/Niiaks/price-tracker/internal/scrapper"
+	"github.com/Niiaks/price-tracker/pkg"
+	"github.com/joho/godotenv"
 )
 
-var dsn = flag.String("dsn", "", "dsn to connect to postgresql")
+var (
+	dsn       = flag.String("dsn", "", "dsn to connect to postgresql")
+	name      = flag.String("name", "", "name of product to scrape")
+	threshold = flag.Float64("threshold", 0.0, "threshold of product to scrape")
+	url       = flag.String("url", "", "url of product to scrape")
+)
 
 func main() {
 	flag.Parse()
@@ -17,13 +27,39 @@ func main() {
 		os.Exit(1)
 	}
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	if *dsn == "" {
 		*dsn = os.Getenv("DATABASE_URL")
 	}
+	if *name == "" {
+		log.Fatal("--name is required")
+	}
+	if *url == "" {
+		log.Fatal("--url is required")
+	}
 
-	_, err := db2.ConnectDB(*dsn)
+	db, err := db2.ConnectDB(*dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	s := scrapper.NewScrapper(db)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	product := pkg.Product{
+		Name:      *name,
+		Threshold: *threshold,
+		Url:       *url,
+	}
+
+	err = s.Scrape(ctx, product)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.26.1
 require (
 	github.com/PuerkitoBio/goquery v1.12.0
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.12.3
 )
 
