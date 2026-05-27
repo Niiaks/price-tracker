c617b741d852163600f978387a9cc7e8fa0a2388
Niiaks <napappoe007@st.ug.edu.gh>
2026-05-27 19:10:28 +0000

    add price history struct and db tags

--- pkg/types.go
+++ pkg/types.go
@@ -3,9 +3,16 @@ package pkg
 import "time"
 
 type Product struct {
-	ID        string
-	Name      string
-	Threshold float64 // alerting when price drops below
-	Url       string  // webpage to scrape
-	CreatedAt time.Time
+	ID        int       `db:"id"`
+	Name      string    `db:"name"`
+	Threshold float64   `db:"threshold"` // alerting when price drops below
+	Url       string    `db:"url"`       // webpage to scrape
+	CreatedAt time.Time `db:"created_at"`
+}
+
+type PriceHistory struct {
+	ID         int       `db:"id"`
+	ProductID  int       `db:"product_id"`
+	Price      float64   `db:"price"`
+	ScrappedAt time.Time `db:"scrapped_at"`
 }
